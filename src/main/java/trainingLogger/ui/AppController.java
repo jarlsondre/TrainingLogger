6f8bd0292e7443a9c6e44db1db16237e47c57a0b
Tomas Gule <Vaagengule@hotmail.com>
2020-09-12 19:54:17 +0200

    #12 Lagt til handler for addDescriptionButton

--- src/main/java/trainingLogger/ui/AppController.java
+++ src/main/java/trainingLogger/ui/AppController.java
@@ -1,5 +1,27 @@
 package trainingLogger.ui;
 
-public class AppController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
 
+public class AppController {
+    @FXML
+    Button addDescriptionButton;
+
+    @FXML
+    TextArea descriptionTextArea;
+
+    @FXML
+    VBox sessionOverviewVBox;
+
+
+    @FXML
+    public void handleAddDescriptionButton() {
+        String description = descriptionTextArea.getText();  // Tar inn teksten fra TextArea
+        descriptionTextArea.setText("");    // Fjerne teksten i TextArea
+        if (!description.isEmpty()){        // Sjekker om strengen er tom, slik at vi ikke må behnadle en tom streng
+            System.out.println(description);
+        }
+    }
 }
