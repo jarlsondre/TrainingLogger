a641bdd0d343a6017091436bb3b3bb9f5dce72b3
Tomas Gule <Vaagengule@hotmail.com>
2020-09-14 16:35:18 +0200

    Ny måte å legge inn text i sessionOverwiev, nå med titte!

--- trainingLogger/src/main/java/trainingLogger/ui/AppController.java
+++ trainingLogger/src/main/java/trainingLogger/ui/AppController.java
@@ -1,9 +1,8 @@
 package trainingLogger.ui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -24,12 +23,26 @@ public class AppController {
         descriptionTextArea.setText("");    // Fjerne teksten i TextArea
         if (!description.isEmpty()){        // Sjekker om strengen er tom, slik at vi ikke må behnadle en tom streng
             //Her kan du gjøre ting med strengen
-            addStringToSessionOverview(description);
+            addStringWithTitleToSessionOverview("Økt numemr x", description); //Bare for å ha en funksjon her midlertidig.
         }
     }
 
 
     public void addStringToSessionOverview(String string){
-        sessionOverviewVBox.getChildren().add(new Text(string));
+         sessionOverviewVBox.getChildren().add(new Text(string));
+    }
+
+
+    //Legger til en titlePane med en vBox som kan fylles med innhold. Her blir descrption lagt til som en label i vboxen
+    public void addStringWithTitleToSessionOverview(String title, String description){
+        VBox vBox = new VBox();
+        vBox.getChildren().add(new Label(description));
+        TitledPane titledPane = new TitledPane("Title" ,vBox);
+        titledPane.setAlignment(Pos.CENTER_LEFT);
+        titledPane.setExpanded(false);
+
+        sessionOverviewVBox.getChildren().add(titledPane);
+
+
     }
 }
