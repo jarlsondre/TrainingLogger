1517b86bcaec6c0c5d5898774ae2c867cf7a9b16
Tomas Gule <Vaagengule@hotmail.com>
2020-09-14 14:39:11 +0200

    #13 Lagt til metoden addStringToSessionOverview

--- trainingLogger/src/main/java/trainingLogger/ui/AppController.java
+++ trainingLogger/src/main/java/trainingLogger/ui/AppController.java
@@ -2,8 +2,10 @@ package trainingLogger.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class AppController {
     @FXML
@@ -22,6 +24,12 @@ public class AppController {
         descriptionTextArea.setText("");    // Fjerne teksten i TextArea
         if (!description.isEmpty()){        // Sjekker om strengen er tom, slik at vi ikke må behnadle en tom streng
             //Her kan du gjøre ting med strengen
+            addStringToSessionOverview(description);
         }
+    }
+
+
+    public void addStringToSessionOverview(String string){
+        sessionOverviewVBox.getChildren().add(new Text(string));
     }
 }
