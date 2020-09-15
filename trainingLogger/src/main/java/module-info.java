15bb0a04f54c76ae8ea4e8bd6622890b9bee3d09
Tomas Gule <Vaagengule@hotmail.com>
2020-09-15 10:23:31 +0200

    Lagt til støtte for Jackson

--- trainingLogger/src/main/java/module-info.java
+++ trainingLogger/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module gr2001 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
     opens trainingLogger.ui to javafx.fxml;
     exports trainingLogger.ui;
