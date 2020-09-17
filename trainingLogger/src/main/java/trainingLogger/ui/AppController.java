27989b343cd1403adb117a900ffe32f83a5a8e7b
Tomas Gule <Vaagengule@hotmail.com>
2020-09-17 09:58:50 +0200

    #18 laget updateSessionOverview for å oppdatere fra sessionLogger

--- trainingLogger/src/main/java/trainingLogger/ui/AppController.java
+++ trainingLogger/src/main/java/trainingLogger/ui/AppController.java
@@ -5,7 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import trainingLogger.core.Session;
-
+import trainingLogger.core.SessionLogger;
 
 
 public class AppController {
@@ -21,17 +21,29 @@ public class AppController {
     @FXML
     VBox sessionOverviewVBox;
 
+    SessionLogger sessionLogger = new SessionLogger();
+
+
+
 
     @FXML
     public void handleAddDescriptionButton() {
         String description = descriptionTextArea.getText();  // Tar inn teksten fra TextArea
         descriptionTextArea.setText("");    // Fjerne teksten i TextArea
         if (!description.isEmpty()){        // Sjekker om strengen er tom, slik at vi ikke må behnadle en tom streng
-            //Her kan du gjøre ting med strengen
-            addSessionToSessionOverview(new Session(description)); //Bare for å ha en funksjon her midlertidig.
+            // Her kan du gjøre ting med strengen
+            // addSessionToSessionOverview(new Session(description)); //Bare for å ha en funksjon her midlertidig.
+            sessionLogger.addSession(new Session(description));
         }
+        updateSessionOverview();
     }
 
+    public void updateSessionOverview(){
+        sessionOverviewVBox.getChildren().clear();
+        for (Session session: sessionLogger.getSessions()){
+            addSessionToSessionOverview(session);
+        }
+    }
 
     //Legger til en titlePane med en vBox som kan fylles med innhold. Her blir descrption lagt til som en label i vboxen
     public void addStringWithTitleToSessionOverview(String title, String description){
@@ -51,17 +63,21 @@ public class AppController {
     @FXML
     public void saveButtonHandler(){
         System.out.println("Save");
-
+        sessionLogger.save();
     }
 
     @FXML
     public void loadButtonHandler(){
         System.out.println("Load");
+        sessionLogger.load();
+        updateSessionOverview();
     }
 
     @FXML
     public void deleteButtonHandler(){
         System.out.println("Delete");
+        sessionLogger.delete();
+        updateSessionOverview();
 
     }
 
