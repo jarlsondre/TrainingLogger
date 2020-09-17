a4ff01f101df39f4f9aceb09812d8f3d6cbb95e0
Tomas Gule <Vaagengule@hotmail.com>
2020-09-17 09:57:32 +0200

    #22 Implementert save() og load(). Opprettet og implementert delete() og getSessions()

--- trainingLogger/src/main/java/trainingLogger/core/SessionLogger.java
+++ trainingLogger/src/main/java/trainingLogger/core/SessionLogger.java
@@ -1,12 +1,15 @@
 package trainingLogger.core;
 
+import trainingLogger.json.FileHandler;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class SessionLogger implements Iterable<Session> {
 
-    private List<Session> sessions = new ArrayList<Session>();
+    private List<Session> sessions = new ArrayList<>();
 
     @Override
     public Iterator<Session> iterator() {
@@ -17,14 +20,38 @@ public class SessionLogger implements Iterable<Session> {
         sessions.add(session);
     }
 
-    public void load(){
+    public List<Session> getSessions() {return this.sessions;}
+
+    public boolean load(){
         // Kaller statisk metode i FileHandler
         // Resultatet er at sessions fylles opp med Session-objekter
+        try {
+            InputStream inputstream = new FileInputStream("session_data.txt");
+            this.sessions = FileHandler.readFromFile(inputstream);
+            return true;
+        } catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
     }
 
-    public void save(){
+    public boolean save(){
         // Kaller statisk metode i FileHandler
         // Resultatet er at Session-objektene i sessions lagres på disk i JSON-format
+        try {
+            File file = new File("session_data.txt");
+            OutputStream fop = new FileOutputStream(file);
+            FileHandler.writeToFile(fop, this.sessions);
+            return true;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    public void delete(){
+        this.sessions = new ArrayList<>();
     }
     
 }
