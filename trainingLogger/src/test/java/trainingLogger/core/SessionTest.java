b9040bb0d2eaabbed5f66d19817984eb10071044
Kristian Bentesønn Fougner <krisfou@stud.ntnu.no>
2020-09-17 09:51:56 +0000

    Skrev test som besto for Session.equlas()

--- trainingLogger/src/test/java/trainingLogger/core/SessionTest.java
+++ trainingLogger/src/test/java/trainingLogger/core/SessionTest.java
@@ -1,6 +1,8 @@
 package trainingLogger.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 
 public class SessionTest {
@@ -9,5 +11,12 @@ public class SessionTest {
     public void testGetDescription_returnsNullInitially(){
         Session session = new Session();
         assertEquals(null, session.getDescription());
+    }
+
+    @Test
+    public void testEquals_SameDescriptionButDifferentDatesAreNotEqual(){
+        Session session1 = new Session("Dette er en økt.");
+        Session session2 = new Session("Dette er en økt.");
+        assertFalse(session1.equals(session2));
     }
 }
