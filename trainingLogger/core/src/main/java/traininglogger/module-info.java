c901d5b92c8e55b20af4dc1b6ac87d3aca2a03aa
Kristian Bentesønn Fougner <krisfou@stud.ntnu.no>
2020-10-01 10:43:22 +0000

    Delt opp prosjektet i to moduler: core og fxui (#25)

---
+++ trainingLogger/core/src/main/java/traininglogger/module-info.java
@@ -0,0 +1,4 @@
+module gr2001.core {
+    requires com.fasterxml.jackson.databind;
+    exports traininglogger.core;
+}
--- trainingLogger/fxui/src/main/java/module-info.java
+++ trainingLogger/fxui/src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module gr2001 {
+module gr2001.fxui {
     requires javafx.controls;
     requires transitive javafx.graphics;
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
+    requires gr2001.core;
 
     opens traininglogger.ui to javafx.fxml;
     exports traininglogger.ui;
-    exports traininglogger.core;
 }
