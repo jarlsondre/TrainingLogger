1096140377587dec237c6e33828889a2c1884aa2
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2020-10-05 22:10:24 +0200

    #35 La til knapp på startskjermen for å komme inn til "ny økt"-skjermen. La til tilbakeknapp for å komme tilbake til startskjermen

--- trainingLogger/fxui/src/main/java/traininglogger/ui/NewSessionScreenController.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/NewSessionScreenController.java
@@ -1,4 +1,13 @@
 package traininglogger.ui;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class NewSessionScreenController {
+
+    @FXML
+    private void switchToStartScreen() throws IOException {
+        App.setRoot("StartScreen");
+    }
 }
