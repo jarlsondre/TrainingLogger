368fe78278ec773beeb8891fe486b4aac56d59f7
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2020-10-05 22:09:33 +0200

    #34 La til knapp for å kunne gå tilbake til startskjermen og overskrift

--- trainingLogger/fxui/src/main/java/traininglogger/ui/SessionScreenController.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/SessionScreenController.java
@@ -1,4 +1,13 @@
 package traininglogger.ui;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class SessionScreenController {
+
+    @FXML
+    private void switchToStartScreen() throws IOException {
+        App.setRoot("StartScreen");
+    }
 }
