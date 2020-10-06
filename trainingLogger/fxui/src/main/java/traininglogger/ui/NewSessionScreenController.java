50f837b87b45e532a9d55273bfa66cbd9b805c08
Tomas Gule <Vaagengule@hotmail.com>
2020-10-06 11:46:35 +0200

    #35 Lagt til mulighet for gå mellom skjermer, lagt til noen bokser

--- trainingLogger/fxui/src/main/java/traininglogger/ui/NewSessionScreenController.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/NewSessionScreenController.java
@@ -1,13 +1,22 @@
 package traininglogger.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class NewSessionScreenController {
 
     @FXML
+    Button newExerciseButton;
+
+    @FXML
     private void switchToStartScreen() throws IOException {
         App.setRoot("StartScreen");
+    }
+
+    @FXML
+    private void switchToNewExerciseScreen() throws IOException {
+        App.setRoot("NewExerciseScreen");
     }
 }
