830403e106e5b79b66a08d7e12a679ba99955697
Tomas Gule <Vaagengule@hotmail.com>
2020-10-06 10:41:36 +0200

    #34 Lagt til vbox

--- trainingLogger/fxui/src/main/java/traininglogger/ui/SessionScreenController.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/SessionScreenController.java
@@ -1,13 +1,20 @@
 package traininglogger.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
 public class SessionScreenController {
 
+    @FXML VBox sessionOverviewVbox;
+
+
     @FXML
     private void switchToStartScreen() throws IOException {
         App.setRoot("StartScreen");
     }
+
+
+
 }
