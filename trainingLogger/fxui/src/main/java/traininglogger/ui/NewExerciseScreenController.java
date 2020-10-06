c50f4bf1d306f55905a942ce7172e0fdefc7fb03
Tomas Gule <Vaagengule@hotmail.com>
2020-10-06 11:47:16 +0200

    #36 Lagt til mulighet for gå mellom skjermer, lagt til noen bokser, og en del knapper.

--- trainingLogger/fxui/src/main/java/traininglogger/ui/NewExerciseScreenController.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/NewExerciseScreenController.java
@@ -1,4 +1,33 @@
 package traininglogger.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
 public class NewExerciseScreenController {
+    @FXML
+    VBox addSetVbox;
+
+    @FXML
+    HBox addSetHbox;
+
+    @FXML
+    TextField weightTextField, repsTextField;
+
+    @FXML
+    private void switchToNewExerciseScreen() throws IOException {
+        App.setRoot("NewSessionScreen");
+    }
+
+    @FXML
+    private void addExerciseButtonHandler() throws IOException {
+        switchToNewExerciseScreen();
+    }
+
+    @FXML
+    private void addSetButtonHandler(){
+    }
 }
