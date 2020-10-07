803cb778dfb358be146ebb5c8c7c3ac432260579
Tomas Gule <Vaagengule@hotmail.com>
2020-10-07 15:54:56 +0200

    #36 Lagt til funksjon som gjør det mulig å legge til flere set i GUI.

--- trainingLogger/fxui/src/main/java/traininglogger/ui/NewExerciseScreenController.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/NewExerciseScreenController.java
@@ -1,6 +1,8 @@
 package traininglogger.ui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -29,6 +31,23 @@ public class NewExerciseScreenController {
 
     @FXML
     private void addSetButtonHandler(){
-        // Legg til ting her
+        addHboxToVbox();
+        weightTextField.setText("");
+        repsTextField.setText("");
+    }
+
+
+    // Legger til en ny hbox i addSetVbox med verdiene fra den nederste Hboxen.
+    private void addHboxToVbox(){
+        FXMLLoader loader = new FXMLLoader();
+        try {
+            Node node  =  loader.load(getClass().getResource("HboxTemplate.fxml").openStream());
+            addSetVbox.getChildren().add(addSetVbox.getChildren().size() - 1,node);
+            //get the controller
+            HboxTemplateController controller = (HboxTemplateController)loader.getController();
+            controller.setTextField(weightTextField.getText(), repsTextField.getText()); //set label
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
