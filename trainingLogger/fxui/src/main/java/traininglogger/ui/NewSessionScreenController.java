d25133e0ed0b505bb7a761f1d92415a7cfee6085
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2020-10-08 16:48:56 +0200

    #35 La til funksjonalitet som viser øvelsene i session-objektet så langt

--- trainingLogger/fxui/src/main/java/traininglogger/ui/NewSessionScreenController.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/NewSessionScreenController.java
@@ -2,13 +2,54 @@ package traininglogger.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import traininglogger.core.Exercise;
+import traininglogger.core.Session;
+import traininglogger.core.SessionLogger;
+import traininglogger.json.FileHandler;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class NewSessionScreenController {
 
     @FXML
-    Button newExerciseButton;
+    VBox exerciseOverviewVbox;
+
+    @FXML
+    public void initialize() {
+        // Her henter vi et session-objekt fra fil og deretter fyller vi på med den siste exercisen. Etter dette vises session-objektet
+        Session session = FileHandler.readSessionFromFile("src/main/resources/session_controller_data.json");
+        Exercise exercise = FileHandler.readExerciseFromFile("src/main/resources/exercise_controller_data.json");
+        File exerciseFile = new File("src/main/resources/exercise_controller_data.json");
+        if (exerciseFile.delete() == false) {
+            System.out.println("Could not delete file");
+        }
+        else {
+            System.out.println("Successfully deleted file");
+        }
+        if (!(exercise == null)) {
+            session.addExercises(exercise);
+        }
+        FileHandler.writeSessionToFile("src/main/resources/session_controller_data.json", session);
+
+        // Vise nåværende session
+        for (Exercise e : session.getListOfExercises()) {
+            Label label = new Label(e.getName() + ": ");
+            String temp = "";
+            for (Integer[] set : e.getSets()) {
+                temp += Integer.toString((Integer) Array.get(set, 0)) +"x"+ Integer.toString((Integer) Array.get(set, 1)) + " ";
+                label.setText(label.getText() + temp);
+                temp = "";
+            }
+            exerciseOverviewVbox.getChildren().add(exerciseOverviewVbox.getChildren().size()-1, label);
+        }
+
+    }
 
     @FXML
     private void switchToStartScreen() throws IOException {
