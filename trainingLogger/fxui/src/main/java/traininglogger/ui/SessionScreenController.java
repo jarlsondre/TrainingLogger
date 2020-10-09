0d7d12547f153583e8f5d819afe664236ae998bb
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2020-10-10 00:45:51 +0200

    #34 La til funksjon som oppdaterer overviewen og viser tidligere økter

--- trainingLogger/fxui/src/main/java/traininglogger/ui/SessionScreenController.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/SessionScreenController.java
@@ -6,16 +6,35 @@ import traininglogger.core.Session;
 import traininglogger.core.SessionLogger;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+
+import static traininglogger.ui.UpdateOverview.sessionToVboxConverter;
 
 public class SessionScreenController {
 
     @FXML VBox sessionOverviewVbox;
 
+    SessionLogger logger;
+
     @FXML
     public void initialize() {
         // we want to fill the VBox with the sesssions we have. First we need to load our sessionLogger-object
-        SessionLogger logger = new SessionLogger();
+        logger = new SessionLogger();
         logger.load();
+        sessionOverviewUpdate();
+    }
+
+    @FXML
+    private void sessionOverviewUpdate() {
+        sessionOverviewVbox.getChildren().clear();
+        for (Session session : logger) {
+            VBox box = sessionToVboxConverter(session);
+            TitledPane titledPane = new TitledPane(session.getDateString(), box);
+            titledPane.setAlignment(Pos.CENTER_LEFT);
+            titledPane.setExpanded(false);
+            sessionOverviewVbox.getChildren().add(0, titledPane);
+
+        }
     }
 
 
