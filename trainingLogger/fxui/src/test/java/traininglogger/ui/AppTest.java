9ec7c3d4da6cc48879c4f1ec6238309f5e2b283b
Kristian Bentesønn Fougner <krisfou@stud.ntnu.no>
2020-10-09 12:19:32 +0000

    (#29) La til kommentarer

--- trainingLogger/fxui/src/test/java/traininglogger/ui/AppTest.java
+++ trainingLogger/fxui/src/test/java/traininglogger/ui/AppTest.java
@@ -3,6 +3,8 @@ package traininglogger.ui;
 import org.testfx.framework.junit5.ApplicationTest;
 
 public class AppTest extends ApplicationTest {
-  
+
+  // Til Kristian: Her testes hovedkontrolleren. Se testfx-branchen
+
   
 }
