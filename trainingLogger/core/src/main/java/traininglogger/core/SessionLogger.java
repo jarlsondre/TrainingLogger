df66085dc920421a5c127b77b6e3df305d93f1d6
Tomas Gule <Vaagengule@hotmail.com>
2020-10-26 13:54:26 +0100

    #46 Fjernet ubrukte metoder i klassene SessionLogger og Session.

--- trainingLogger/core/src/main/java/traininglogger/core/SessionLogger.java
+++ trainingLogger/core/src/main/java/traininglogger/core/SessionLogger.java
@@ -22,45 +22,8 @@ public class SessionLogger implements Iterable<Session> {
     sessions.add(session);
   }
 
-  /**
-   *Kaller statisk metode i FileHandler
-   *Resultatet er at sessions fylles opp med Session-objekter 
-  public boolean load() {
-    try {
-      InputStream inputstream = new FileInputStream(fileLocationString);
-      this.sessions = FileHandler.readFromFile(inputstream);
-      return true;
-    } catch (Exception e) {
-      System.out.println(e);
-      return false;
-    }
-  }
-
-  Kaller statisk metode i FileHandler
-  Resultatet er at Session-objektene i sessions lagres på disk i JSON-format
-
-  public boolean save() {
-    try {
-      File file = new File(fileLocationString);
-      OutputStream fop = new FileOutputStream(file);
-      FileHandler.writeToFile(fop, this.sessions);
-      return true;
-    } catch (Exception e) {
-      System.out.println(e);
-      return false;
-    }
-  }*/
-
   public void deleteAll() {
     this.sessions = new ArrayList<>();
-  }
-
-  public void deleteLast() {
-    this.sessions.remove(this.sessions.size()-1);
-  }
-
-  public Session getLastSession() {
-    return this.sessions.get(this.sessions.size()-1);
   }
 
 }
