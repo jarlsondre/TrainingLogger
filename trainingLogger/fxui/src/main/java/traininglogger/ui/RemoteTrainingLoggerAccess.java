0e10425bc21b4df08ed35d28744762fe171cabc9
Kristian Bentesønn Fougner <krisfou@stud.ntnu.no>
2020-10-28 14:22:03 +0000

    fjernet ubrukte imports

--- trainingLogger/fxui/src/main/java/traininglogger/ui/RemoteTrainingLoggerAccess.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/RemoteTrainingLoggerAccess.java
@@ -3,14 +3,10 @@ package traininglogger.ui;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collection;
 
 import traininglogger.core.Session;
 import traininglogger.core.SessionLogger;
