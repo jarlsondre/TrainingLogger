f491cda01e79e20f55f2a31d0ebe6e48b715bf48
Tomas Gule <Vaagengule@hotmail.com>
2020-11-06 13:38:42 +0100

    #59 Lagt til et Map med rekorder, og metoder for å legge oppdatere/fjerne rekorder.

--- trainingLogger/core/src/main/java/traininglogger/core/SessionLogger.java
+++ trainingLogger/core/src/main/java/traininglogger/core/SessionLogger.java
@@ -1,16 +1,42 @@
 package traininglogger.core;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 
 /** 
- * SessionLogger objektet inneholder en liste med sessions.
+ * SessionLogger objektet inneholder en liste med sessions og et Map med rekorder.
  */
 public class SessionLogger implements Iterable<Session> {
 
   private List<Session> sessions = new ArrayList<>();
+  private Map<String, Double> records = new HashMap<>();
+
+  public void updateRecordsWithSession(Session session){
+    for (Exercise exercise : session){
+      String name  = exercise.getName();
+      double maxWeight = 0;
+      for (Set set : exercise){
+        maxWeight = Math.max(maxWeight, set.getWeight());
+      }
+      if (records.containsKey(name) && (maxWeight > records.get(name))){
+        records.replace(name, maxWeight);
+      }
+      else{
+        records.put(name, maxWeight);
+      }
+    }
+  }
+
+  public void initializeRecords(){
+    records.clear();
+    for(Session session : this){
+      updateRecordsWithSession(session);
+    }
+  }
+
+  public void deleteRecords(){
+    records.clear();
+  }
 
   @Override
   public Iterator<Session> iterator() {
@@ -18,11 +44,14 @@ public class SessionLogger implements Iterable<Session> {
   }
 
   public void addSession(Session session) {
+    updateRecordsWithSession(session);
     sessions.add(session);
+    System.out.println(records);
   }
 
   public void deleteAll() {
     this.sessions.clear();
+    this.deleteRecords();
   }
 
   public String toString() {
