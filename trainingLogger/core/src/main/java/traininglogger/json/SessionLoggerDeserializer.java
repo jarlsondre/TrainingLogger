43516914fde80b374799bfe64d1224f7f1d68de8
Tomas Gule <Vaagengule@hotmail.com>
2020-11-07 20:47:41 +0100

    #59 Lagt til støtte for rekorder. Endret på måten sessionObjekter blir lagt til.

--- trainingLogger/core/src/main/java/traininglogger/json/SessionLoggerDeserializer.java
+++ trainingLogger/core/src/main/java/traininglogger/json/SessionLoggerDeserializer.java
@@ -3,12 +3,18 @@ package traininglogger.json;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import traininglogger.core.Session;
 import traininglogger.core.SessionLogger;
 
@@ -34,20 +40,25 @@ public class SessionLoggerDeserializer extends JsonDeserializer<SessionLogger> {
    * @return det rekonstruerte SessionLogger-objektet hvis deserialiseringen lykkes, null ellers.
    */
   public SessionLogger deserialize(JsonNode jsonNode) {
+    Map<String, Double> records;
+    List<Session> sessionList = new ArrayList<>();
+
     if (jsonNode instanceof ObjectNode) {
       ObjectNode objectNode = (ObjectNode) jsonNode;
       JsonNode sessionsNode = objectNode.get("sessions");
       if (sessionsNode instanceof ArrayNode) {
         ArrayNode sessions = (ArrayNode) sessionsNode;
-        SessionLogger sessionLogger = new SessionLogger();
         for (JsonNode element : sessions) {
           Session session = this.sessionDeserializer.deserialize(element);
           if (session != null) {
-            sessionLogger.addSession(session);
+            sessionList.add(session);
           }
         }
-        sessionLogger.initializeRecords();
-        return sessionLogger;
+        JsonNode recordsNode = objectNode.get("records");
+        ObjectMapper mapper = new ObjectMapper();
+        records = mapper.convertValue(recordsNode, new TypeReference<>() {
+        });
+        return new SessionLogger(sessionList, records);
       }
     }
     return null;
