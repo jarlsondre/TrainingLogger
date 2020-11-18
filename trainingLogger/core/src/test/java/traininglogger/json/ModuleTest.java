5229933baba1fe2c570e2421d6c443416db76b8b
Kristian Bentesønn Fougner <krisfou@stud.ntnu.no>
2020-11-18 15:23:37 +0000

    skrevet tester av (de)serialiserer og TrainingLoggerPersistence ved samspill

--- trainingLogger/core/src/test/java/traininglogger/json/ModuleTest.java
+++ trainingLogger/core/src/test/java/traininglogger/json/ModuleTest.java
@@ -79,39 +79,14 @@ public class ModuleTest {
       fail();
     }
   }
+
   // Bruker konkatinering fordi gitpod-IDEen slår seg vrang hvis strengen står på én linje:
   private final static String sessionLoggerWithOneSession = "{\"sessions\":[{\"stringDescription\":\"Uten trening gir livet ingen mening!\",\"date\":\"12/12/1989 15:15\",\"exercises\":[{\"name\":\"Testpress\",\"sets\":[{\"repetitions\":5,\"weight\":100.0},"
       + "{\"repetitions\":5,\"weight\":97.5},{\"repetitions\":5,\"weight\":87.5}]},{\"name\":\"Testbøy\",\"sets\":[{\"repetitions\":8,\"weight\":130.0},{\"repetitions\":8,\"weight\":127.5},{\"repetitions\":7,\"weight\":125.0}]}]}],\"records\":{\"Testpress\":100.0,\"Testbøy\":130.0}}";
 
   @Test
   public void testSerializers() {
-    Session session = new Session();
-
-    Set set0 = new Set(5, 100);
-    Set set1 = new Set(5, 97.5);
-    Set set2 = new Set(5, 87.5);
-    Set[] sets1 = new Set[3];
-    sets1[0] = set0;
-    sets1[1] = set1;
-    sets1[2] = set2;
-    Exercise exercise1 = new Exercise("Testpress", sets1);
-    session.addExercises(exercise1);
-
-    Set set3 = new Set(8, 130);
-    Set set4 = new Set(8, 127.5);
-    Set set5 = new Set(7, 125);
-    Set[] sets2 = new Set[3];
-    sets2[0] = set3;
-    sets2[1] = set4;
-    sets2[2] = set5;
-    Exercise exercise2 = new Exercise("Testbøy", sets2);
-    session.addExercises(exercise2);
-
-    session.setDate("12/12/1989 15:15");
-    session.setDescription("Uten trening gir livet ingen mening!");
-
-    SessionLogger sessionLogger = new SessionLogger();
-    sessionLogger.addSession(session);
+    SessionLogger sessionLogger = createSessionLoggerWithOneSession();
     try {
       String stringProducedBySerializer = mapper.writeValueAsString(sessionLogger);
       assertEquals(sessionLoggerWithOneSession, stringProducedBySerializer);
@@ -164,5 +139,67 @@ public class ModuleTest {
     } catch (JsonProcessingException e) {
       fail();
     }
+  }
+
+  @Test
+  public void testSerializersAndDeserializers() {
+    SessionLogger originalSessionLogger = createSessionLoggerWithOneSession();
+    try {
+      String sessionLoggerAsString = mapper.writeValueAsString(originalSessionLogger);
+      SessionLogger recreatedSessionLogger = mapper.readValue(sessionLoggerAsString, SessionLogger.class);
+      assertEquals(originalSessionLogger.getRecords().get("Testpress"), recreatedSessionLogger.getRecords().get("Testpress"));
+      assertEquals(originalSessionLogger.getRecords().get("Testbøy"), recreatedSessionLogger.getRecords().get("Testbøy"));
+      assertTrue(originalSessionLogger.iterator().hasNext());
+      Iterator<Session> originalIterator = originalSessionLogger.iterator();
+      Session originalSession = originalIterator.next();
+      assertTrue(recreatedSessionLogger.iterator().hasNext());
+      Iterator<Session> recreatedIterator = recreatedSessionLogger.iterator();
+      Session recreatedSession = recreatedIterator.next();
+      assertEquals(originalSession.getDescription(), recreatedSession.getDescription());
+      assertEquals(originalSession.getDate(), recreatedSession.getDate());
+      assertFalse(originalIterator.hasNext());
+      assertFalse(recreatedIterator.hasNext());
+      assertTrue(originalSession.iterator().hasNext());
+      Iterator<Exercise> originalSessionIterator = originalSession.iterator();
+      assertTrue(recreatedSession.iterator().hasNext());
+      Iterator<Exercise> recreatedSessionIterator = recreatedSession.iterator();
+      assertTrue(ComparisonHelper.equalExercise(originalSessionIterator.next(), recreatedSessionIterator.next()));
+      assertTrue(ComparisonHelper.equalExercise(originalSessionIterator.next(), recreatedSessionIterator.next()));
+      assertFalse(originalSessionIterator.hasNext());
+      assertFalse(recreatedSessionIterator.hasNext());
+    } catch (JsonProcessingException e) {
+      fail();
+    }
+  }
+
+  private SessionLogger createSessionLoggerWithOneSession() {
+    Session session = new Session();
+
+    Set set0 = new Set(5, 100);
+    Set set1 = new Set(5, 97.5);
+    Set set2 = new Set(5, 87.5);
+    Set[] sets1 = new Set[3];
+    sets1[0] = set0;
+    sets1[1] = set1;
+    sets1[2] = set2;
+    Exercise exercise1 = new Exercise("Testpress", sets1);
+    session.addExercises(exercise1);
+
+    Set set3 = new Set(8, 130);
+    Set set4 = new Set(8, 127.5);
+    Set set5 = new Set(7, 125);
+    Set[] sets2 = new Set[3];
+    sets2[0] = set3;
+    sets2[1] = set4;
+    sets2[2] = set5;
+    Exercise exercise2 = new Exercise("Testbøy", sets2);
+    session.addExercises(exercise2);
+
+    session.setDate("12/12/1989 15:15");
+    session.setDescription("Uten trening gir livet ingen mening!");
+
+    SessionLogger sessionLogger = new SessionLogger();
+    sessionLogger.addSession(session);
+    return sessionLogger;
   }
 }
