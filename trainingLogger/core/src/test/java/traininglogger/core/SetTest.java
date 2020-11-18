0a0bb13f6d70b9e6e655bf74294e6fd61d8791a0
Kristian Bentesønn Fougner <krisfou@stud.ntnu.no>
2020-11-18 18:06:33 +0000

    #74 droppet asignment i test, instansiering er tilstrekkelig

--- trainingLogger/core/src/test/java/traininglogger/core/SetTest.java
+++ trainingLogger/core/src/test/java/traininglogger/core/SetTest.java
@@ -8,24 +8,24 @@ public class SetTest {
   @Test
   public void constructorTest() {
     try {
-      Set set1 = new Set(1001, 100);
+      new Set(1001, 100);
       fail("Det skal ikke være lov å lage et sett med flere enn 1000 repetisjoner");
     } catch (Exception e) {
       // Gjør ingenting
     }
     try {
-      Set set2 = new Set(8, 1001);
+      new Set(8, 1001);
       fail("Det skal ikke være lov å lage et sett med mer enn 1000kg");
     } catch (Exception e) {
       // GJør ingenting
     }
     try {
-      Set set3 = new Set(1000, 1000);
+      new Set(1000, 1000);
     } catch (Exception e) {
       fail("Det skal være lov å lage et sett med 1000 repetisjoner og 1000kg");
     }
     try {
-      Set set4 = new Set(10, 100);
+      new Set(10, 100);
     } catch (Exception e) {
       fail("Det skal være lov å lage et sett med 10 repetisjoner og 100kg");
     }
