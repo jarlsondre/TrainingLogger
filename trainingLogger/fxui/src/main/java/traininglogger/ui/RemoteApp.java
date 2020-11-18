0e1c0933151f6ff7e5dd1403a84276400b0a2f54
Bendik Gjermundrød Holter <bendikgh@stud.ntnu.no>
2020-11-18 13:09:28 +0000

    Endret navnet på vinduet.

--- trainingLogger/fxui/src/main/java/traininglogger/ui/RemoteApp.java
+++ trainingLogger/fxui/src/main/java/traininglogger/ui/RemoteApp.java
@@ -17,6 +17,7 @@ public class RemoteApp extends Application {
   public void start(final Stage stage) throws IOException {
     Scene scene = new Scene(new FXMLLoader(App.class.getResource("RemoteApp.fxml")).load());
     stage.setScene(scene);
+    stage.setTitle("Training Logger");
     stage.show();
   }
 
